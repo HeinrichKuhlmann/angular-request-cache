29387ab11885d534aab5a1d4fc95497461358f91
Heinrich Kuhlmann <HeinrichKuhlmann@gmail.com>
2012-12-15 15:34:30 +0100

    yeoman init

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var angularRequestCacheApp = angular.module('angularRequestCacheApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
