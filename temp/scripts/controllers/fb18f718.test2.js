9d7cbcb6453c421e56738ffc1a381a615f0d1e2f
Heinrich Kuhlmann <HeinrichKuhlmann@gmail.com>
2013-01-09 15:00:10 +0100

    dist

---
+++ temp/scripts/controllers/fb18f718.test2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angularRequestCacheApp.controller('Test2Ctrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
